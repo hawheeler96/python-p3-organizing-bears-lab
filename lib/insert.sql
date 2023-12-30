40302845a36ccc31bc1d2a69d16ceb5f8641c3c5
Holly Wheeler <hollyawheeler.96@gmail.com>
2023-12-30 10:15:42 -0800

    lab complete

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, sex, temperament, color, alive) VALUES (1, 'Mr. Chocolate', 5, 'M', 'Docile', 'Brown', 1);
+INSERT INTO bears (id, name, age, sex, temperament, color, alive) VALUES (2, 'Rowdy', 2, 'F', 'Rowdy', 'Grey', 1);
+INSERT INTO bears (id, name, age, sex, temperament, color, alive) VALUES (3, 'Tabitha', 3, 'F', 'Docile', 'Black', 1);
+INSERT INTO bears (id, name, age, sex, temperament, color, alive) VALUES (4, 'Seargeant Brown', 7, 'M', 'Grumpy', 'Brown', 0);
+INSERT INTO bears (id, name, age, sex, temperament, color, alive) VALUES (5, 'Melissa', 4, 'F', 'Docile', 'Black', 1);
+INSERT INTO bears (id, name, age, sex, temperament, color, alive) VALUES (6, 'Grinch', 8, 'M', 'Grumpy', 'White', 1);
+INSERT INTO bears (id, name, age, sex, temperament, color, alive) VALUES (7, 'Wendy', 2, 'F', 'Rowdy', 'Grey', 0);
+INSERT INTO bears (id, age, sex, temperament, color, alive) VALUES (8, 5, 'M', 'Grumpy', 'Grey', 1);
